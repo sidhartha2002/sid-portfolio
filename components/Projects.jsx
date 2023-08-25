4a635c7ab199a32c792e963241a6ec6b4d31cd5a
sidhartha2002 <mohantysidhartha2002@gmail.com>
2023-08-25 14:32:40 +0530

    &pos

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -40,29 +40,32 @@ const Projects = () => {
               <p className="font-titleFont text-textGreen text-sm tracking-wide">
                 Featured Project
               </p>
-              <h3 className="text-2xl font-bold">eatiz Web</h3>
+              <h3 className="text-2xl font-bold">Serverless e-Commerce App</h3>
               <p className="bg-[#112240] text-sm md:text-base p-2 md:p-6 rounded-md text-center xl:text-right">
-                Lorem Ipsum is simply dummy text of the printing and typesetting
-                industry. Lorem Ipsum has been the industry's standard dummy
-                text ever since the 1500s, when an unknown printer took a galley
-                of type and scrambled it to make a type specimen book.
+                This project is a serverless ecommerce app crafted with Next.js,
+                Tailwind CSS, PlanetScale for the database, Clerk for
+                authentication, and Stripe for checkout. It highlights my skills
+                in UI/UX design, serverless architecture, and integrating
+                diverse technologies for seamless user experience.
               </p>
               <ul className="flex gap-2 justify-between text-xs md:text-sm font-titleFont -tracking-wide md:gap-5 text-textDark">
-                <li>NextJS</li>
+                <li>NextJS 13</li>
                 <li>Typescript</li>
-                <li>Tailwind CSS</li>
-                <li>Vercel</li>
+                <li>Tailwind</li>
+                <li>PlanetScale</li>
+                <li>Stripe</li>
+                <li>Clerk</li>
               </ul>
               <div className="text-2xl flex gap-4">
                 <a
-                  href="#"
+                  href="https://github.com/sidhartha2002/ecommerce-app"
                   target="_blank"
                   className="hover:text-textGreen duration-300 "
                 >
                   <RiGithubLine />
                 </a>
                 <a
-                  href="#"
+                  href="https://ecommerce-app-eight-gamma.vercel.app/"
                   target="_blank"
                   className="hover:text-textGreen duration-300 "
                 >
@@ -76,7 +79,7 @@ const Projects = () => {
         <div className="w-full flex flex-col items-center justify-center gap-28 mt-10">
           <div className="flex flex-col xl:flex-row-reverse gap-6">
             <a
-              href="#"
+              href=""
               target="_blank"
               className="w-full xl:w-1/2 h-auto relative group"
             >
@@ -92,12 +95,13 @@ const Projects = () => {
               <p className="font-titleFont text-textGreen text-sm tracking-wide">
                 Featured Project
               </p>
-              <h3 className="text-2xl font-bold">EShop Web</h3>
+              <h3 className="text-2xl font-bold">Issue Tracker</h3>
               <p className="bg-[#112240] text-sm md:text-base p-2 xl:-mr-16 md:p-6 rounded-md text-center xl:text-right">
-                Lorem Ipsum is simply dummy text of the printing and typesetting
-                industry. Lorem Ipsum has been the industry's standard dummy
-                text ever since the 1500s, when an unknown printer took a galley
-                of type and scrambled it to make a type specimen book.
+                This project is a minimalistic web application to keep track
+                your labeled issues using Next.js and Tailwind CSS. Leveraging
+                React Query for efficient data fetching and state management,
+                this app offers features like automatic synchronization, issue
+                filtering, addition of issues, and labeling functionalities.
               </p>
               <ul className="text-xs md:text-sm font-titleFont -tracking-wide flex gap-2 md:gap-5 justify-between text-textDark">
                 <li>NextJS</li>
@@ -107,66 +111,14 @@ const Projects = () => {
               </ul>
               <div className="text-2xl flex gap-4">
                 <a
-                  href="#"
+                  href="https://github.com/sidhartha2002/ecommerce-app"
                   target="_blank"
                   className="hover:text-textGreen duration-300 "
                 >
                   <RiGithubLine />
                 </a>
                 <a
-                  href="#"
-                  target="_blank"
-                  className="hover:text-textGreen duration-300 "
-                >
-                  <RxOpenInNewWindow />
-                </a>
-              </div>
-            </div>
-          </div>
-        </div>
-        {/* Project 3 */}
-        <div className="w-full flex flex-col items-center justify-center gap-28 mt-10">
-          <div className="flex flex-col xl:flex-row gap-6">
-            <a
-              href="https://famous-sunshine-6cd5fe.netlify.app/"
-              target="_blank"
-              className="w-full xl:w-1/2 h-auto relative group"
-            >
-              <div>
-                <Image
-                  className="w-full h-full object-contain"
-                  src={foodWeb2}
-                  alt="foodWeb1"
-                />
-              </div>
-            </a>
-            <div className="w-full xl:w-1/2 flex flex-col gap-6 items-center xl:items-end text-right xl:-ml-16 z-10">
-              <p className="font-titleFont text-textGreen text-sm tracking-wide">
-                Featured Project
-              </p>
-              <h3 className="text-2xl font-bold">Parallax Web</h3>
-              <p className="bg-[#112240] text-sm md:text-base p-2 md:p-6 rounded-md text-center xl:text-right">
-                Lorem Ipsum is simply dummy text of the printing and typesetting
-                industry. Lorem Ipsum has been the industry's standard dummy
-                text ever since the 1500s, when an unknown printer took a galley
-                of type and scrambled it to make a type specimen book.
-              </p>
-              <ul className="text-xs md:text-sm font-titleFont -tracking-wide flex gap-2 md:gap-5 justify-between text-textDark">
-                <li>Html</li>
-                <li>Javascript</li>
-                <li>CSS</li>
-                <li>Netlify</li>
-              </ul>
-              <div className="text-2xl flex gap-4">
-                <a
-                  href="https://github.com/sidhartha2002/Parallax-WebPage"
-                  target="_blank"
-                  className="hover:text-textGreen duration-300 "
-                >
-                  <RiGithubLine />
-                </a>
-                <a
-                  href="https://famous-sunshine-6cd5fe.netlify.app/"
+                  href="https://ecommerce-app-eight-gamma.vercel.app/"
                   target="_blank"
                   className="hover:text-textGreen duration-300 "
                 >
